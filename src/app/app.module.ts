e7f78a0e47cf58399799bc7e185eabe5536a77e2
Scott Lewis <scottcl@outlook.com>
2021-01-01 17:35:46 -0500

    Init commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AmplifyUIAngularModule } from '@aws-amplify/ui-angular';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,    
+    AmplifyUIAngularModule
   ],
   imports: [
     BrowserModule
